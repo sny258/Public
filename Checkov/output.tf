645b3e759f781add463a7a324645f7ef9a12b1cc
sny258 <sanjay.c.verma@accenture.com>
2022-06-04 19:57:11 +0530

    Add checkov

---
+++ Checkov/output.tf
@@ -0,0 +1,9 @@
+output "vnet_id" {
+  value       = module.network.vnet_id
+  description = "The Vnet Name"
+}
+
+output "vnet_name" {
+  value       = module.network.vnet_name
+  description = "The Vnet Name"
+}
---
+++ Checkov/variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  type        = string
+  description = "Region in Azure"
+  default     = "eastus"
+}
+
+variable "prefix" {
+  type        = string
+  description = "prefix for naming"
+  default     = "tacos"
+}
